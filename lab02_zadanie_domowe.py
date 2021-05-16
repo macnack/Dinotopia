15bb613051a979599ede489d3d24c21c593306d2
Maciek <maciej.p.krupka@student.put.poznan.pl>
2021-05-16 12:05:50 +0200

    instrukcja lab02

---
+++ lab02_zadanie_domowe.py
@@ -0,0 +1,55 @@
+# wczytanie potrzebnych bibliotek:
+import roboticstoolbox as rtb
+import numpy as np
+import math
+from spatialmath import *
+from spatialmath.base import *
+from spatialmath.base.symbolic import *
+import time
+
+
+# ...
+
+# definicje funkcji:
+#def przykład_1():
+
+
+# your code
+
+#def zadanie_1():
+    # your code
+    #...
+    #robot = ...
+    #return robot
+
+
+#def zadanie_2(robot):
+
+
+# your code
+
+#def zadanie_3(robot):
+
+
+# your code
+
+#def zadanie_4():
+
+
+# your code
+
+#def zadanie_5():
+
+
+# your code
+
+# ...
+
+# wykonywanie wybranej funkcji
+if __name__ == '__main__':
+# przykład_1()
+# robot = zadanie_1()
+# zadanie_2(robot)
+# zadanie_3(robot)
+# zadanie_4()
+# zadanie_5()
